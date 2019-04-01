2f15171692c51bb322c5188743a2ba04952756d1
samir-dell <samirthaker2020>
2019-04-01 19:39:36 -0400

    spinner example

--- app/src/main/java/com/example/datap_demo/Main2Activity.java
+++ app/src/main/java/com/example/datap_demo/Main2Activity.java
@@ -2,10 +2,14 @@ package com.example.datap_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
-
+    String[] country = {"India", "Canada", "USA", "UK"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,25 @@ public class Main2Activity extends AppCompatActivity {
                 Toast.makeText(this,name,Toast.LENGTH_LONG).show();
             }
         }
+
+        Spinner spinner = (Spinner) findViewById(R.id.spcountrylist);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, country);
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // On selecting a spinner item
+                String item = parent.getItemAtPosition(position).toString();
+
+                // Showing selected spinner item
+                Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
