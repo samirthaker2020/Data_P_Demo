3136227e368285da1e1339b6b4509964da46fa01
samir-dell <samirthaker2020>
2019-04-01 19:17:01 -0400

    saving data using shared preference

---
+++ app/src/main/java/com/example/datap_demo/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.datap_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle mbundle= getIntent().getExtras();
+        if(mbundle!=null)
+        {
+            if(mbundle.containsKey("name"))
+            {
+                String name=mbundle.getString("name");
+                Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
